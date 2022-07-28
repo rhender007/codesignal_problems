fabd0bc9c0bf639cafaecba2a43d417c65b549b9
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2022-07-28 10:58:30 -0400

    Update FizzBuzz.py

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -11,3 +11,8 @@ def fizzbuzz(n):
         else:
             ret.append(i)
     return ret
+
+test.describe('Fizzbuzz')
+test.it('Should fizzify 10 numbers correctly')
+expected = [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz']
+test.assert_equals(fizzbuzz(10), expected)
